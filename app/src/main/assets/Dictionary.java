485a03f4597d44358ca0a3ec751582c1854f9899
CaptainPrintf <atticusram@gmail.com>
2024-10-26 17:10:11 -0500

    Csv file and dictionary

---
+++ app/src/main/assets/Dictionary.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Map;
+public class Dictionary {
+    Map<String, String> dictionary;
+
+    public Dictionary(){
+        dictionary = new HashMap<>()
+
+    }
+    publoc void add(String word, String definition){}
+
+    public void setDictionary(Map<String, String> dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public Map<String, String> getDictionary() {
+        return dictionary;
+    }
+    public
+
+}
