a0ab0b4f59a9b3a3c4adf7c785cf278fd20f57e2
zcsager <zachary.sager@my.utsa.edu>
2024-10-26 17:48:02 -0500

    Created load quiz bank method for lesson and a sample xml file

--- app/src/main/java/com/example/bankucation/model/Lesson.java
+++ app/src/main/java/com/example/bankucation/model/Lesson.java
@@ -1,5 +1,11 @@
 package com.example.bankucation.model;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Scanner;
 
 public class Lesson {
     private ArrayList<Dictionary> dictionaryBank;
@@ -36,7 +42,44 @@ public class Lesson {
         quizBank.remove(index);
     }
 
-    public void loadQuizBank(String fileName) {
+    // Load arraylist of questions from XML file
+    public static ArrayList<Question> loadQuizBank(InputStream inputStream) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            ArrayList<Question> quizBank = new ArrayList<Question>();
 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // new quizbank
+                if(line.equals("<QuizBank>")) {
+                    while (!(line = reader.readLine()).trim().equals("</QuizBank>")) {
+                        line = reader.readLine().trim();
+                        String questionText = "";
+                        String answerText = "";
+                        ArrayList<String> otherAnswers = new ArrayList<String>();
+                        // new question
+                        if (line.equals("<Question>")) {
+
+                            if ((line = reader.readLine().trim()).equals("<QuestionText>")) {
+                                questionText = reader.readLine().trim();
+                            }
+                            reader.readLine();
+                            if ((line = reader.readLine().trim()).equals("<AnswerText>")) {
+                                answerText = reader.readLine().trim();
+                            }
+                            reader.readLine();
+                            while ((line = reader.readLine().trim()).equals("<OtherChoice>")) {
+                                otherAnswers.add(reader.readLine().trim());
+                                reader.readLine();
+                            }
+                        }
+                        quizBank.add(new Question(questionText, answerText, otherAnswers));
+                    }
+                }
+            }
+            return quizBank;
+        } catch (Exception e)    {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
