970ab0d483ecd6283b8abb9f91c0523bedcde53a
zcsager <zachary.sager@my.utsa.edu>
2024-10-26 15:19:22 -0500

    Lesson basic variables

--- app/src/main/java/com/example/bankucation/model/Lesson.java
+++ app/src/main/java/com/example/bankucation/model/Lesson.java
@@ -1,4 +1,17 @@
 package com.example.bankucation.model;
+import java.util.ArrayList;
 
 public class Lesson {
+    private dictionaryBank <ArrayList>Dictionary();
+    private quizBank <ArrayList>Question();
+
+    // Constructor
+    public Lesson(<ArrayList>Dictionary() dictionaryBank, <ArrayList>Question() quizBank) {
+        this.dictionaryBank = dictionaryBank;
+        this.quizBank = quizBank;
+    }
+
+    // Dictionary Bank
+
+    // Question Bank
 }
