668fcbc7e1165a9964639bf6d7945c63b50ba338
zcsager <zachary.sager@my.utsa.edu>
2024-10-26 16:37:30 -0500

    Lesson basic methods

--- app/src/main/java/com/example/bankucation/model/Lesson.java
+++ app/src/main/java/com/example/bankucation/model/Lesson.java
@@ -2,16 +2,37 @@ package com.example.bankucation.model;
 import java.util.ArrayList;
 
 public class Lesson {
-    private dictionaryBank <ArrayList>Dictionary();
-    private quizBank <ArrayList>Question();
+    private ArrayList<Dictionary> dictionaryBank;
+    private ArrayList<Question> quizBank;
 
     // Constructor
-    public Lesson(<ArrayList>Dictionary() dictionaryBank, <ArrayList>Question() quizBank) {
+    public Lesson(ArrayList<Dictionary> dictionaryBank, ArrayList<Question> quizBank) {
         this.dictionaryBank = dictionaryBank;
         this.quizBank = quizBank;
     }
 
     // Dictionary Bank
+    public ArrayList<Dictionary> getDictionaryBank() {
+        return dictionaryBank;
+    }
+    public void setDictionaryBank(ArrayList<Dictionary> dictionaryBank) {
+        this.dictionaryBank = dictionaryBank;
+    }
 
-    // Question Bank
+    // Quiz Bank
+    public ArrayList<Question> getQuizBank() {
+        return quizBank;
+    }
+    public void setQuizBank(ArrayList<Question> quizBank) {
+        this.quizBank = quizBank;
+    }
+    public void addQuestion(Question newQuestion) {
+        quizBank.add(newQuestion);
+    }
+    public void removeQuestion(Question oldQuestion) {
+        quizBank.remove(oldQuestion);
+    }
+    public void removeQuestion(int index) {
+        quizBank.remove(index);
+    }
 }
